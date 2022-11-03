db3641052a1a6c232c027b46726d399fe9381260
Texi Schaeffer <texirae.schaeffer@gmail.com>
2022-11-03 12:18:24 -0500

    completed mvp

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -5,12 +5,20 @@
 */
 import axios from 'axios';
 
-axios.get('https://api.githum.com/users/texirae');
+const followersArray = ['tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'];
+
+for(let i = 0; i<followersArray.length; i++){
+  getGitCard(followersArray[i]);
+}
+
+function getGitCard(username){
+  axios.get(`https://api.githum.com/users/${username}`);
   .then(resp => {
-    console.log(resp);
+    document.querySelector('.cards').appendChild(githubCard(resp.data));
   })
 
   .catch(err => console.error(err));
+}
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -37,16 +45,50 @@ axios.get('https://api.githum.com/users/texirae');
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
-
 function githubCard(gitInfo) {
   const card = document.createElement('div');
   const img = document.createElement('img');
   const cardInfo = document.createElement('div');
   const name = document.createElement('h3');
-  const username = document.createElement('p');
+  const login = document.createElement('p');
   const location = document.createElement('p');
   const profile = document.createElement('p');
+  const profileLink = document.createElement('a');
+  const followers = document.createElement('p');
+  const following = document.createElement('p');
+  const bio = document.createElement('p');
+
+  card.classList.add('card');
+  cardInfo.classList.add('card-info');
+  name.classList.add('name');
+  login.classList.add('username');
+
+  img.src = gitInfo.avatar_url;
+  img.alt = "github user"
+  name.textContent = gitInfo.name;
+  login.textContent = gitInfo.login;
+  location.textContent = gitInfo.location;
+  profile.textContent = "Profile";
+  profileLink.textContent = "Link to profile";
+  profileLink.href = gitInfo.html_url;
+  followers.textContent = `Followers: ${gitInfo.followers}`;
+  following.textContent = `Following: ${gitInfo.following}`;
+  bio.textContent = gitInfo.bio;
+
+  card.appendChild(img);
+  card.appendChild(cardInfo);
+  cardInfo.appendChild(name);
+  cardInfo.appendChild(login);
+  cardInfo.appendChild(location);
+  cardInfo.appendChild(profile);
+  profile.appendChild(profileLink);
+  cardInfo.appendChild(followers);
+  cardInfo.appendChild(following);
+  cardInfo.appendChild(bio);
+
+  return card;
+
+
 }
 
 /*
